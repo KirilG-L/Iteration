010aed1b774c4816cd3ba0b61689f26e8ba1b491
Kiril <kiril.gudim@gmail.com>
2020-01-12 19:20:41 +0200

    Lab 4, exercise 6

--- Exercises/6-matrix.js
+++ Exercises/6-matrix.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const max = matrix => {
-  // Use nested for loop to find max value in 2d matrix
-  // For example max([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-  // should return 9
+  let result = 0;
+  for (const row of matrix) {
+    for (const item of row) {
+      if (item > result) result = item;
+    }
+  }
+  return result;
 };
 
 module.exports = { max };
