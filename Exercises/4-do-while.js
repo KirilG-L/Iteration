8d30d8c0dc9b9f22b967dbcf24dee3b2de8a9078
Kiril <kiril.gudim@gmail.com>
2020-01-12 19:14:04 +0200

    Lab 4, exercise 4

--- Exercises/4-do-while.js
+++ Exercises/4-do-while.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use do..while loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let i = 0;
+  let res = 0;
+  do {
+    if (args[i]) {
+      res += args[i];
+    }
+    i++;
+  } while (i < args.length);
+    return res;
 };
 
 module.exports = { sum };
